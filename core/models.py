63aeb0a01ccb2a40fcabcde9c14dbd66efc2c121
Vontrelll <darvinvontrell@gmail.com>
2026-04-21 22:11:17 -0500

    v1-c2: I have completed the models for event, kid, family inside of Django and have registered them to the database provide by django.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+GENDER_CHOICES = [("M", "Male"), ("F", "Female")]
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length = 100)
+    kid_attending = models.ForeignKey("Kid", on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return f"{self.name} for {self.kid_attending} at {self.start_time}"
+
+
+class Kid(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(choices = GENDER_CHOICES, max_length=1)
+    family = models.ForeignKey("Family", on_delete=models.CASCADE)
+    parent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Family(models.Model):
+    family_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    parents = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    
